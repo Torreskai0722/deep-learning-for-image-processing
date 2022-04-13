40fe20332de9877748a68f2bccaba7303b4c3167
Torreskai0722 <liangkai@wayne.edu>
2022-04-13 15:15:46 -0400

    commit

--- pytorch_segmentation/deeplab_v3/predict.py
+++ pytorch_segmentation/deeplab_v3/predict.py
@@ -7,7 +7,10 @@ from torchvision import transforms
 import numpy as np
 from PIL import Image
 
-from src import deeplabv3_resnet50
+# from src import deeplabv3_resnet50
+import torchvision.models as models
+
+from torch.profiler import profile, record_function, ProfilerActivity
 
 
 def time_synchronized():
@@ -18,33 +21,34 @@ def time_synchronized():
 def main():
     aux = False  # inference time not need aux_classifier
     classes = 20
-    weights_path = "./save_weights/model_29.pth"
-    img_path = "./test.jpg"
-    palette_path = "./palette.json"
-    assert os.path.exists(weights_path), f"weights {weights_path} not found."
+    # weights_path = "./save_weights/model_29.pth"
+    img_path = "./test.jpeg"
+    # palette_path = "./palette.json"
+    # assert os.path.exists(weights_path), f"weights {weights_path} not found."
     assert os.path.exists(img_path), f"image {img_path} not found."
-    assert os.path.exists(palette_path), f"palette {palette_path} not found."
-    with open(palette_path, "rb") as f:
-        pallette_dict = json.load(f)
-        pallette = []
-        for v in pallette_dict.values():
-            pallette += v
+    # assert os.path.exists(palette_path), f"palette {palette_path} not found."
+    # with open(palette_path, "rb") as f:
+    #     pallette_dict = json.load(f)
+    #     pallette = []
+    #     for v in pallette_dict.values():
+    #         pallette += v
 
     # get devices
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
 
     # create model
-    model = deeplabv3_resnet50(aux=aux, num_classes=classes+1)
+    # model = deeplabv3_resnet50(aux=aux, num_classes=classes+1)
+    model = models.segmentation.deeplabv3_resnet50(pretrained=True)
 
     # delete weights about aux_classifier
-    weights_dict = torch.load(weights_path, map_location='cpu')['model']
-    for k in list(weights_dict.keys()):
-        if "aux" in k:
-            del weights_dict[k]
+    # weights_dict = torch.load(weights_path, map_location='cpu')['model']
+    # for k in list(weights_dict.keys()):
+    #     if "aux" in k:
+    #         del weights_dict[k]
 
-    # load weights
-    model.load_state_dict(weights_dict)
+    # # load weights
+    # model.load_state_dict(weights_dict)
     model.to(device)
 
     # load image
@@ -61,21 +65,29 @@ def main():
 
     model.eval()  # 进入验证模式
     with torch.no_grad():
-        # init model
-        img_height, img_width = img.shape[-2:]
-        init_img = torch.zeros((1, 3, img_height, img_width), device=device)
-        model(init_img)
+        with profile(activities=[ProfilerActivity.CPU], record_shapes=True, with_modules=True) as prof:
+            with record_function("model_inference"):
+                # init model
+                img_height, img_width = img.shape[-2:]
+                init_img = torch.zeros((1, 3, img_height, img_width), device=device)
+                model(init_img)
 
-        t_start = time_synchronized()
-        output = model(img.to(device))
-        t_end = time_synchronized()
-        print("inference+NMS time: {}".format(t_end - t_start))
+                t_start = time_synchronized()
+                output = model(img.to(device))
+                t_end = time_synchronized()
+                print("inference+NMS time: {}".format(t_end - t_start))
 
-        prediction = output['out'].argmax(1).squeeze(0)
-        prediction = prediction.to("cpu").numpy().astype(np.uint8)
-        mask = Image.fromarray(prediction)
-        mask.putpalette(pallette)
-        mask.save("test_result.png")
+                prediction = output['out'].argmax(1).squeeze(0)
+                prediction = prediction.to("cpu").numpy().astype(np.uint8)
+                mask = Image.fromarray(prediction)
+                # mask.putpalette(pallette)
+                mask.save("test_result.png")
+        print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+        print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=10))
+        print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
+        print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+        
+        prof.export_chrome_trace("trace.json")
 
 
 if __name__ == '__main__':
